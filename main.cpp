54148379389cc090633c30a22d23398a2392ee1f
alex nakagama <alexnakagama4@gmail.com>
2026-05-25 01:29:51 -0300

    added comments to how compile code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+// g++ main.cpp -o main
+// this commands compiles my code into a binary executable file named main
+// the -o flag is to change the executable file name
+
 int main() {
   cout << "Hello world!" << endl;
   return 0;
