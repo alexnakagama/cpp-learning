82606f7aca873fc0181a22f697019172b8f034f3
alex nakagama <alexnakagama4@gmail.com>
2026-05-25 12:03:28 -0300

    added comments explaining code

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // import iostream library
 
 // g++ main.cpp -o main
 // this commands compiles my code into a binary executable file named main
 // the -o flag is to change the executable file name
 
 int main() {
-  cout << "Hello world!" << endl;
-  return 0;
+    std::cout << "Hello world!\n";
+    // std::endl creates a new line (\n) but also empties the buffer
+    // \n only inserts a new line and the buffer is flushed automatically later
+    // std::endl when we need the output to appear immediately like debugging or
+    // logging info
+    std::cout << "My name is alex\n";
+    std::cout << "Im 20 years old." << std::endl;
+
+    return 0;
 }
