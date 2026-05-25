2b6156e348af1c774c8a7f549f36bdeeb80f758e
alex nakagama <alexnakagama4@gmail.com>
2026-05-25 13:19:29 -0300

    arrays

---
+++ arrays.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    // an array contains data that has a relation
+    // they are not dynamic
+    int arrayNumbers[3];
+
+    // here we are initializing the array
+    int matrix[3]{20, 15, 40};
+
+    std::cout << matrix[0] << std::endl;
+    std::cout << "matrix[1]: " << matrix[1] << std::endl;
+
+    matrix[1] = 253;
+
+    std::cout << "matrix[1] ahora es: " << matrix[1] << std::endl;
+
+    return 0;
+}
