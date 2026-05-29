2a548bd1e6681f3d08257fbf520702535a8e3814
Alex Nakagama <alexn@50-0-0-104.static.sonic.net>
2026-05-29 00:33:53 -0300

    memory4

--- pointers.cpp
+++ pointers.cpp
@@ -6,32 +6,46 @@ int num{55};
 void miFunction(int *num) { *num = *num + 15; }
 
 int main() {
-    // pointers is a variable that stores the memory address of another variable
+  // pointers is a variable that stores the memory address of another variable
 
-    // dynamic memory management
-    // pass by reference
-    // direct memory access
+  // dynamic memory management
+  // pass by reference
+  // direct memory access
 
-    int var{10};
+  int var{10};
 
-    // declare the pointer *ptr
-    int *ptr;
+  // declare the pointer *ptr
+  int *ptr;
 
-    // store the memory address of the var variable
-    ptr = &var;
+  // store the memory address of the var variable
+  ptr = &var;
 
-    std::cout << var << "\n";  // print the value of var
-    std::cout << &var << "\n"; // print the memory address of var
-    std::cout << ptr << "\n";  // print the memory address of var stored in
-                               // the pointer ptr
+  std::cout << var << "\n";  // print the value of var
+  std::cout << &var << "\n"; // print the memory address of var
+  std::cout << ptr << "\n";  // print the memory address of var stored in
+                             // the pointer ptr
 
-    std::cout << *ptr
-              << "\n"; // print the value of who the pointer is pointing to
+  std::cout << *ptr
+            << "\n"; // print the value of who the pointer is pointing to
 
-    *ptr = 15; // change the var value from the pointer
-    std::cout << *ptr << "\n";
+  *ptr = 15; // change the var value from the pointer
+  std::cout << *ptr << "\n";
 
-    // call miFunction with the memory address of num
-    miFunction(&num);
-    std::cout << num << "\n"; // instead of returning 55 it returns 70
+  // call miFunction with the memory address of num
+  miFunction(&num);
+  std::cout << num << "\n"; // instead of returning 55 it returns 70
+
+  // pointer to a constant value
+  // the value the pointer is pointing to cannot change
+  // the pointer can change who is pointing to
+
+  // creates a constant = 15
+  const int constNum{15};
+  // creates a pointer pointing to memory address constNum
+  const int *ptrToConst{&constNum};
+  // *ptrToConst = 20; this would not compile
+  const int salary{2000};
+
+  // the pointer can change the memory address its pointing to
+  ptrToConst = &salary;
 }
