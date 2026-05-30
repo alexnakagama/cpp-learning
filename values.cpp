eddfd7af151ed6f1481580f3d9972c582c684fc2
alexnakagama <alexnakagama4@gmail.com>
2026-05-30 19:50:46 -0300

    left and right values

---
+++ values.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  // an L-Value (left value) its an expression that represents a memory location
+  // that means that l value has a memory address
+
+  // the z is l-value
+  int z{15};
+
+  // the R-Value (right value) its the value 15. represents a temporal value
+  // without a persistent location in memory
+}
