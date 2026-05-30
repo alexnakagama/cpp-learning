8886087a13b265cbe52f75aa5167f3fc7eaa1e7f
alexnakagama <alexnakagama4@gmail.com>
2026-05-30 19:42:42 -0300

    passing as reference

--- references.cpp
+++ references.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 
+void exchangeValues(int &a, int &b) {
+  int temp = a;
+
+  a = b;
+  b = temp;
+}
+
 int main() {
   // references is an alias for an already existing variable
   // its not a separated object and it doesnt store a memory address
   // it just acts as the original object
+
+  int num1{5};
+  int num2{10};
+
+  std::cout << "Num 1: " << num1 << "\n";
+  std::cout << "Num 2: " << num2 << "\n";
+
+  exchangeValues(num1, num2);
+  std::cout << "--------------------------\n";
+
+  std::cout << "Num 1: " << num1 << "\n";
+  std::cout << "Num 2: " << num2 << "\n";
 }
