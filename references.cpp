86b904e4ad15f261dc82c12e194b3d1c08e7a0b8
alexnakagama <alexnakagama4@gmail.com>
2026-05-30 19:27:40 -0300

    references

---
+++ references.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+  // references is an alias for an already existing variable
+  // its not a separated object and it doesnt store a memory address
+  // it just acts as the original object
+}
