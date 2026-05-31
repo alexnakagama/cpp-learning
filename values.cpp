c0d31b655e01dad17add14326e41197f99d51e8d
alexnakagama <alexnakagama4@gmail.com>
2026-05-31 02:37:33 -0300

    values

--- values.cpp
+++ values.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+#include <utility> // required for std::move
+
+void incrementNumber(int &&num) { num += 5; }
+
 int main() {
   // an L-Value (left value) its an expression that represents a memory location
   // that means that l value has a memory address
@@ -7,6 +11,12 @@ int main() {
   // the z is l-value
   int z{15};
 
+  std::cout << "The number before function: " << z << "\n";
+
   // the R-Value (right value) its the value 15. represents a temporal value
   // without a persistent location in memory
+
+  // using move value to transform the z left value to a right value
+  incrementNumber(std::move(z));
+  std::cout << "The number after function: " << z << "\n";
 }
