af333e003be554d90c99cd714496bd1a24a3fa92
alexnakagama <alexnakagama4@gmail.com>
2026-05-31 17:27:12 -0300

    values

--- values.cpp
+++ values.cpp
@@ -2,6 +2,11 @@
 
 #include <utility> // required for std::move
 
+std::string addText(std::string &&z) {
+  z += "Text added";
+  return z;
+}
+
 void incrementNumber(int &&num) { num += 5; }
 
 int main() {
@@ -19,4 +24,9 @@ int main() {
   // using move value to transform the z left value to a right value
   incrementNumber(std::move(z));
   std::cout << "The number after function: " << z << "\n";
+
+  std::string text{"Hello world"};
+  std::cout << "Text before: " << text << "\n";
+  std::string text2 = addText(std::move(text));
+  std::cout << "Text after: " << text2 << "\n";
 }
